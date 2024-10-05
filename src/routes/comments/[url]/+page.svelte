047dc01d0013199b8ee00ba82422cdb2d001510e
SkyfallWasTaken <mahadkalam1@proton.me>
2024-10-05 23:00:27 +0100

    Remove unused stuff

--- src/routes/comments/[url]/+page.svelte
+++ src/routes/comments/[url]/+page.svelte
@@ -2,7 +2,6 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	console.log('Component data:', data);
 </script>
 
 <h1 class="text-3xl font-bold mb-2">{data.comments.length} comments on {data.url}</h1>
